76dfa67136fdfd44093d3e99fc6a5632fb7bb778
FmBrands <dupegift2002@gmail.com>
2023-04-30 20:01:33 +0100

    API Integration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+//let apiKey = "8a181b06553e24t5o65bbf7366a4c3f3 ";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
+//console.log(a
+//let apiKey = "203fa770242fcd2b9555d832a88ea567";
+//let city = "London";
+//let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
+let city = "New-York";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+console.log(apiUrl);
+console.log(apiUrl);
