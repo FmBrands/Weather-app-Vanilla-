273bd51d2cec5b1e69d941f41ff28aec66ff8804
FmBrands <dupegift2002@gmail.com>
2023-04-30 21:54:52 +0100

    Updated js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-//let apiKey = "8a181b06553e24t5o65bbf7366a4c3f3 ";
-//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function displayTemperature(response) {
+  console.log(response.data);
+  console.log(response.data.temperature);
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+}
 
-//console.log(a
-//let apiKey = "203fa770242fcd2b9555d832a88ea567";
-//let city = "London";
-//let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
 let city = "New-York";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-console.log(apiUrl);
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
